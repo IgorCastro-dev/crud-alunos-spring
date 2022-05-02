003d404b6f6043f85728eee90295b7e9ae07457e
Igor Castro <igorjunqueira19@gmail.com>
2022-05-02 15:02:53 -0300

    criando a entidade aluno

---
+++ src/main/java/com/spring/alunos/model/entity/Aluno.java
@@ -0,0 +1,54 @@
+package com.spring.alunos.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "alunos")
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "first_name")
+    @NotBlank
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotBlank
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
