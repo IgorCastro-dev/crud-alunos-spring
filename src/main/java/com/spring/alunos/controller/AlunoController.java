40eca9bfdb60019d1bd2d54e3eda7b9dee94a0bc
Igor Castro <igorjunqueira19@gmail.com>
2022-05-06 11:07:41 -0300

    criando o form de aluno

--- src/main/java/com/spring/alunos/controller/AlunoController.java
+++ src/main/java/com/spring/alunos/controller/AlunoController.java
@@ -2,13 +2,19 @@ package com.spring.alunos.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.spring.alunos.model.entity.Aluno;
 import com.spring.alunos.model.service.AlunoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AlunoController {
@@ -22,5 +28,20 @@ public class AlunoController {
         List<Aluno> alunos = as.getAllAlunos();
         mv.addObject("alunos", alunos);
         return mv;
+    }
+
+    @GetMapping("/aluno/novo")
+    public String formaluno(){
+        return "formaluno";
+    }
+    @PostMapping("/aluno/novo")
+    public String savealuno(@Valid Aluno aluno, BindingResult results, RedirectAttributes attributes){
+        if(results.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "verifique se os campos form preenchidos");
+            return "redirect:/aluno/novo";
+        }else{
+        as.saveAluno(aluno);
+        return "redirect:/alunos";
+        }
     }
 }
